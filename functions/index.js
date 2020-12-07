bc5f6b11b1d9eebe5d4922109916e88406c84f5b
ozawa_shotaro <39585292+tk-ozawa@users.noreply.github.com>
2020-12-07 10:34:45 +0900

    フォーマッター有効化 (#37)
    * change: delete toplevel package.json
    
    * add: format settings
    
    * change: delete .vscode.copy
    
    * add: readme.md & フォーマッターの有効化についてreadmeに記載
    
    * change: restore top level package.json

--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,24 @@
-'use strict';
-const functions = require('firebase-functions');
+"use strict";
+const functions = require("firebase-functions");
 const config = functions.config();
 
-const { App, ExpressReceiver } = require('@slack/bolt');
+const { App, ExpressReceiver } = require("@slack/bolt");
 const expressReceiver = new ExpressReceiver({
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-    token: process.env.SLACK_BOT_TOKEN,
-    endpoints: '/events',
-    processBeforeResponse: true
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN,
+  endpoints: "/events",
+  processBeforeResponse: true,
 });
 const app = new App({
-    receiver: expressReceiver,
-    token: config.slack.bot_token,
-    processBeforeResponse: true
+  receiver: expressReceiver,
+  token: config.slack.bot_token,
+  processBeforeResponse: true,
 });
 app.error(console.log);
 
 // Slackの処理はここから書き始める
-app.message('hello', async ({ message, say }) => {
-    await say(`Hey there <@${message.user}>!`);
+app.message("hello", async ({ message, say }) => {
+  await say(`Hey there <@${message.user}>!`);
 });
 // Slackの処理はここまで
 
