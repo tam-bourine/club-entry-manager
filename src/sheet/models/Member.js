a5a1009a15636fd51d91e35790fd4b78cce73dc5
kenta chihara <kenta.chihara@tam-bourine.co.jp>
2020-12-21 18:18:30 +0900

    clean: modelsで直接シートを操作するように変更

--- src/sheet/models/Member.js
+++ src/sheet/models/Member.js
@@ -1,9 +1,13 @@
 class Member {
   constructor(clubName) {
-    this.sheet = new Sheet(clubName);
+    this.sheet = SpreadSheetApp.getActiveSpreadSheet.getSheetByName(clubName);
   }
 
-  static create() {
-    
+  static create(params) {
+    /*
+    values = objectToArray(params) paramsをキーだけ無くした配列に変換
+    行を追加して挿入
+    return 成功したらtrue, 失敗したらfalse
+    */
   }
 }
