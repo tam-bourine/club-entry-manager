a25023fdd126b4a04be944a7516e5c8a75c92e01
tk-ozawa <ozawa.shotaro0114@gmail.com>
2021-01-21 17:43:10 +0900

    update: postNewClubのts化 & interface追加

--- src/functions/api/postNewClub.ts
+++ src/functions/api/postNewClub.ts
@@ -1,5 +1,4 @@
-'use strict';
-const axios = require('axios');
+import axios from 'axios'
 // json-serverにPOSTする際のURL
 const url = 'http://localhost:3000/club';
 // Slackのワークフローから送られてくる創部申請データのダミー
@@ -11,15 +10,23 @@ const newClubData = {
   member_2nd: 'kaka'
 }
 
+export interface ClubData {
+  name: string,
+  description: string,
+  captain: string,
+  member_1st: string,
+  member_2nd: string,
+}
+
 // 今回はテストなので送られたダミーデータは「db.json」に格納される
-async function postNewClub(clubData) {
+async function postNewClub({ name, description, captain, member_1st, member_2nd }: ClubData) {
   try {
     const res = await axios.post(url, {
-      "club_name": clubData.name,
-      "club_description": clubData.description,
-      "captain": clubData.captain,
-      "collaborator1": clubData.member_1st,
-      "collaborator2": clubData.member_2nd
+      "club_name": name,
+      "club_description": description,
+      "captain": captain,
+      "collaborator1": member_1st,
+      "collaborator2": member_2nd
     });
     console.log(res.data);
   } catch(err) {
