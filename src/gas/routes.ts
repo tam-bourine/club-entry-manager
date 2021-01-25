38c0ba9e3542c222b1f6deb756f2c04fec711d6b
kenta chihara <kenta.chihara@tam-bourine.co.jp>
2021-01-25 15:57:54 +0900

    feat: add routing

---
+++ src/gas/routes.ts
@@ -0,0 +1,51 @@
+/* global ContentService MembersController ClubsController */
+// eslint-disable-next-line no-unused-vars
+function doGet(e) {
+  const action = e.parameter;
+  let res;
+  switch (action) {
+    case "get": {
+      res = ClubsController.get();
+      break;
+    }
+    default: {
+      res = {
+        status: 404,
+        message: "404 Not Found",
+        success: false,
+      };
+      break;
+    }
+  }
+  return ContentService.createTextOutput(JSON.stringify(res)).setMimeType(ContentService.MimeType.JSON);
+}
+
+// eslint-disable-next-line no-unused-vars
+function doPost(e) {
+  const action = e.parameter;
+  const params = JSON.parse(e.postDataAsString());
+  let res;
+  switch (action) {
+    case "regist": {
+      res = ClubsController.regist(params);
+      break;
+    }
+    case "approve": {
+      res = ClubsController.approve(params);
+      break;
+    }
+    case "join": {
+      res = MembersController.join(params);
+      break;
+    }
+    default: {
+      res = {
+        status: 404,
+        message: "404 Not Found",
+        success: false,
+      };
+      break;
+    }
+  }
+  return ContentService.createTextOutput(JSON.stringify(res)).setMimeType(ContentService.MimeType.JSON);
+}
