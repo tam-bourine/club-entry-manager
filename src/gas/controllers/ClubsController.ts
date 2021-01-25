7363e93f6eac5376c0570fe7f72a82d1ed8f3d3d
kenta chihara <kenta.chihara@tam-bourine.co.jp>
2021-01-25 15:57:54 +0900

    feat: add routing

---
+++ src/gas/controllers/ClubsController.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+class ClubsController {
+  static get() {
+    return [];
+  }
+
+  static regist(params: object) {
+    return params;
+  }
+
+  static approve(params: object) {
+    return params;
+  }
+}
---
+++ src/gas/controllers/MembersController.ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-unused-vars
+class MembersController {
+  static join(parmas) {
+    return parmas;
+  }
+}
