1cd26abbbab504e6ad13916b2882056fd8e3ce50
chocopc123 <0215ryuuku@gmail.com>
2021-01-26 17:28:09 +0900

    feature: 受け取った部活動情報を部活動一覧シートに追加

--- src/gas/controllers/ClubsController.ts
+++ src/gas/controllers/ClubsController.ts
@@ -5,6 +5,9 @@ class ClubsController {
   }
 
   static approve(params: object) {
+    const sheet = SpreadsheetApp.openById('129KSPJnEEE6do0mtV19gSr-IbfI8xDdDBjSmAhUrUN8');
+    const today = new Date();
+    sheet.appendRow([params.clubId, params.clubName, params.captainName, params.collaboratorName1st, params.collaboratorName2nd, today, '', params.captainId, params.collaboratorId1st, params.collaboratorId2nd]);
     return params;
   }
 }
