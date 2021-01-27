2d6ddd4a41c0d744a54b160b256d4cd2d37dc469
Asato Nago <nago@tam-bourine.co.jp>
2021-01-27 09:59:41 +0900

    fix: 🐛 eslint gas plugin

--- src/gas/controllers/ClubsController.ts
+++ src/gas/controllers/ClubsController.ts
@@ -10,18 +10,29 @@ class ClubsController {
       const sheet_id: string = PropertiesService.getScriptProperties().getProperty("SPREAD_SHEET_ID");
       const sheet = SpreadsheetApp.openById(sheet_id);
       const today: Date = new Date();
-      sheet.appendRow([params.clubId, params.clubName, params.captainName, params.collaboratorName1st, params.collaboratorName2nd, today, '', params.captainId, params.collaboratorId1st, params.collaboratorId2nd]);
+      sheet.appendRow([
+        params.clubId,
+        params.clubName,
+        params.captainName,
+        params.collaboratorName1st,
+        params.collaboratorName2nd,
+        today,
+        "",
+        params.captainId,
+        params.collaboratorId1st,
+        params.collaboratorId2nd,
+      ]);
       response = {
-        "status": 201,
-        "message": "201 Created",
-        "success": true
-      }
-    } catch(error) {
+        status: 201,
+        message: "201 Created",
+        success: true,
+      };
+    } catch (error) {
       response = {
-        "status": 500,
-        "message": "500 Internal Server Error",
-        "success": false
-      }
+        status: 500,
+        message: "500 Internal Server Error",
+        success: false,
+      };
     }
     return response;
   }
