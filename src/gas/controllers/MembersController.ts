256cfa829c4cedd8486e296111e2f8e10595de8d
kenta chihara <kenta.chihara@tam-bourine.co.jp>
2021-01-29 11:56:58 +0900

    refactor: remove comment rules

--- src/gas/controllers/MembersController.ts
+++ src/gas/controllers/MembersController.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-class MembersController {
+export default class MembersController {
   static join(parmas) {
     return parmas;
   }
--- src/gas/types/ParameterInterface.ts
+++ src/gas/types/ParameterInterface.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-interface ParameterInterface {
+export interface ParameterInterface {
   clubId: string;
   clubName: string;
   captainName: string;
