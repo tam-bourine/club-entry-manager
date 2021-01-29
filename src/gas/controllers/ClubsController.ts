8d4d567d9d8ef9f8b79140e30274fc73df24e68e
shuta13 <hollystarsun@gmail.com>
2021-01-29 15:50:28 +0900

    fix: type error

--- src/gas/controllers/ClubsController.ts
+++ src/gas/controllers/ClubsController.ts
@@ -8,28 +8,27 @@ export class ClubsController {
   }
 
   static approve(params: ParameterInterface) {
-    let response: ResponseInterface;
     try {
-      const sheetId: string = PropertiesService.getScriptProperties().getProperty("SPREAD_SHEET_ID");
-      const sheet = SpreadsheetApp.openById(sheetId);
-      const today: Date = new Date();
-      sheet.appendRow([
-        params.clubId,
-        params.clubName,
-        params.captainName,
-        params.collaboratorName1st,
-        params.collaboratorName2nd,
-        today,
-        "",
-        params.captainId,
-        params.collaboratorId1st,
-        params.collaboratorId2nd,
-      ]);
-      response = new Util().makeSuccess({ status: 201, message: "201 Created" });
+      const sheetId = PropertiesService.getScriptProperties().getProperty("SPREAD_SHEET_ID");
+      if (sheetId) {
+        const sheet = SpreadsheetApp.openById(sheetId);
+        const today = new Date();
+        sheet.appendRow([
+          params.clubId,
+          params.clubName,
+          params.captainName,
+          params.collaboratorName1st,
+          params.collaboratorName2nd,
+          today,
+          "",
+          params.captainId,
+          params.collaboratorId1st,
+          params.collaboratorId2nd,
+        ]);
+        return new Util().makeSuccess({ status: 201, message: "201 Created" });
+      }
     } catch (error) {
-      response = new Util().makeError({ status: 500, message: "500 Internal Server Error" });
-      console.error({ response });
+      return new Util().makeError({ status: 500, message: "500 Internal Server Error" });
     }
-    return response;
   }
 }
--- src/gas/types/ResponseInterface.ts
+++ src/gas/types/ResponseInterface.ts
@@ -1,5 +1,5 @@
 export interface ResponseInterface {
   status: number;
   message: string;
-  success: boolean;
+  success?: boolean;
 }
