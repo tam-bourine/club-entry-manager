03d6601dc486f13495fad735f777394913cd1b03
shuta13 <hollystarsun@gmail.com>
2021-02-01 18:16:41 +0900

    chore: add dotenv

--- src/gas/controllers/Clubs/GetController.ts
+++ src/gas/controllers/Clubs/GetController.ts
@@ -1,5 +1,6 @@
 import ResponseInterface from "../../types/ResponseInterface";
 import Utils from "../../shared/Response";
+import Constants from "../../shared/Constants";
 
 export default class GetController {
   constructor() {}
@@ -8,20 +9,24 @@ export default class GetController {
 
   show() {
     try {
-      const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("部活動一覧");
-      const data = sheet?.getDataRange().getValues();
-      const clubs: ResponseInterface["clubs"] = [];
-      data?.map((value) => {
-        clubs.push({
-          id: value[0],
-          name: value[1],
+      const sheetTabName =
+        Constants.SpreadSheet.SHEET_TAB_NAME || PropertiesService.getScriptProperties().getProperty("SHEET_TAB_NAME");
+      if (sheetTabName) {
+        const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetTabName);
+        const data = sheet?.getDataRange().getValues();
+        const clubs: ResponseInterface["clubs"] = [];
+        data?.map((value) => {
+          clubs.push({
+            id: value[0],
+            name: value[1],
+          });
         });
-      });
-      return this.util.makeSuccess({
-        status: 200,
-        message: "200 OK",
-        clubs: clubs,
-      });
+        return this.util.makeSuccess({
+          status: 200,
+          message: "200 OK",
+          clubs: clubs,
+        });
+      }
     } catch (error) {
       console.error({ error });
       return this.util.makeError({ status: 500, message: "500 Internal Server Error" });
