e8846b9b77ff958f303ae93da15ddbb9c845c058
shuta13 <hollystarsun@gmail.com>
2021-02-05 17:47:25 +0900

    feat: 承認時の処理

--- src/gas/models/Club/ApproveModel.ts
+++ src/gas/models/Club/ApproveModel.ts
@@ -1,7 +1,10 @@
+import Response from "../../shared/Response";
 import ApproveInterface from "../../shared/types/ApproveInterface";
 import ApproveView from "../../views/Club/ApproveView";
 
 export default class ApproveModel {
+  private res = new Response();
+
   private view = new ApproveView();
 
   updateClub(params: ApproveInterface) {
@@ -9,18 +12,43 @@ export default class ApproveModel {
      * Approved : 公認セルを更新
      * Rejected : RegistModel.addClub で追加したクラブの Row を削除
      */
+    const { is_approved } = params;
 
-    if (params.is_approved) {
-      this.updateApprovedClub();
+    if (is_approved) {
+      this.updateApprovedClub(params);
     } else {
-      this.deleteRejectedClub();
+      this.deleteRejectedClub(params);
     }
 
     // @ts-ignore
     return this.view.provide(params);
   }
 
-  private updateApprovedClub() {}
+  private updateApprovedClub(params: ApproveInterface) {
+    const { clubId } = params;
+    try {
+      const sheetTabName = PropertiesService.getScriptProperties().getProperty("SHEET_TAB_NAME");
+      if (sheetTabName) {
+        const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetTabName);
+        const data = sheet?.getDataRange().getValues();
+        data?.map((value, index) => {
+          /**
+           * value[0] : id
+           */
+          if (value[0] === clubId) {
+            /**
+             * 列7 : 公認
+             */
+            sheet?.getRange(7, index).setValue("checked");
+          }
+        });
+        return this.view.provide(this.res.created);
+      } else return this.view.provide(this.res.internalServer);
+    } catch (error) {
+      console.error({ error });
+      return this.view.provide(this.res.internalServer);
+    }
+  }
 
-  private deleteRejectedClub() {}
+  private deleteRejectedClub(params: ApproveInterface) {}
 }
