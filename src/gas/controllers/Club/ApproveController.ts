03dc9746386d2dc5984b712c3e10ccacbb2f42df
shuta13 <hollystarsun@gmail.com>
2021-02-05 14:04:20 +0900

    chore: add comment

--- src/gas/controllers/Club/ApproveController.ts
+++ src/gas/controllers/Club/ApproveController.ts
@@ -4,10 +4,16 @@ import ApproveInterface from "../../types/ApproveInterface";
 export default class ApproveController {
   private approve = new ApproveModel();
 
+  /**
+   * Add New Club（CreateClub 3, 4）
+   */
   create(params: ApproveInterface) {
     return this.approve.addClub(params);
   }
 
+  /**
+   * Confirm That Created Club Approved（CreateClub 6）
+   */
   update(params: ApproveInterface) {
     return this.approve.confirmIsApproved(params);
   }
