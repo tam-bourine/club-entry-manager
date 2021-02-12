44d9c84a5118318414c9b0bc4437014e882995c6
shuta13 <hollystarsun@gmail.com>
2021-02-12 16:25:40 +0900

    fix

--- src/functions/kibela/fetchUserById.ts
+++ src/functions/kibela/fetchUserById.ts
@@ -1,4 +1,5 @@
-// TODO: SlackApp 側で SlackID から Email を取得
-export const getEmail = () => {};
+// TODO: SlackApp 側で SlackId から Email を取得、はダメそう
+// Kibela Id で検索する方向で
+export const getKibelaId = () => {};
 
-export const fetchUserByEmail = () => {};
+export const fetchUserByKibelaId = () => {};
