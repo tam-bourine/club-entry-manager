db48b97c01565bd18d406ddd68f94bfa275f79d3
tk-ozawa <ozawa.shotaro0114@gmail.com>
2021-02-17 15:46:59 +0900

    update: portの環境変数をparseIntに変更

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -20,6 +20,6 @@ app.error((err) => {
 useNewClubCommand(app, process.env.SLACK_APPROVAL_CHANNEL_ID!);
 
 (async () => {
-  await app.start(Number(process.env.BOLT_PORT) || 3000);
+  await app.start(parseInt(process.env.BOLT_PORT!, 10) ?? 3000);
   console.log("⚡️ Bolt app is running!");
 })();
