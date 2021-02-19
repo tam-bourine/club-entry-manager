9e0f1c7efc50978ac0b64c321b7b928fec9fe266
did0es <38882716+shuta13@users.noreply.github.com>
2021-02-19 19:05:09 +0900

    POST approve で1行目が消し飛ぶバグの修正 (#121)
    * wip: map してる部分で壊れた(全部201 Created になる)
    
    * wip: 動いてない
    
    * fix: なおした
    
    * fix: 尽くした
    
    * fix: delete negative comment
    
    * fix: remove isApproved member
    
    * fix: add comment

--- src/gas/models/Club/ApproveModel.ts
+++ src/gas/models/Club/ApproveModel.ts
@@ -3,6 +3,11 @@ import Response from "../../shared/Response";
 import ApproveInterface from "../../shared/types/ApproveInterface";
 import ApproveView from "../../views/Club/ApproveView";
 
+interface UpdateApprovedClubParams {
+  clubId: ApproveInterface["clubId"];
+  isApproved: ApproveInterface["isApproved"];
+}
+
 export default class ApproveModel {
   private res = new Response();
 
@@ -12,57 +17,65 @@ export default class ApproveModel {
 
   updateClub(params: ApproveInterface) {
     /**
-     * Approved : 公認セルを更新
-     * Rejected : RegistModel.addClub で追加したクラブの Row を削除
+     * Approved : 公認セルを TRUE で更新
+     * Rejected : 公認セルを FALSE で更新
      */
     const { clubId, isApproved } = params;
 
-    if (isApproved) {
-      return this.updateApprovedClub(clubId);
-    }
-    return this.deleteRejectedClub(clubId);
+    // return this.view.provide({ status: 2000000000, message: `${clubId} was isApproved: ${isApproved}` });
+
+    return this.updateApprovedClub({ clubId, isApproved });
   }
 
-  private updateApprovedClub(clubId: ApproveInterface["clubId"]) {
+  private updateApprovedClub(params: UpdateApprovedClubParams) {
+    const { clubId, isApproved } = params;
     try {
       const sheetTabName = PropertiesService.getScriptProperties().getProperty("SHEET_TAB_NAME");
       if (sheetTabName) {
         const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetTabName);
         const data = sheet?.getDataRange().getValues();
-        const isApproved = true;
-        data?.map((value, index) => {
-          /**
-           * value[0] : id
-           */
-          /**
-           * 列7 : 公認
-           */
+        /**
+         * @description
+         *  &&
+         *    @see https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Logical_AND
+         *
+         *  `rowData.includes && rowIndex` によって、 clubId が行に含まれている場合
+         *  && の右の rowIndex(クラブが含まれる行の index)が return される
+         *  clubId が行に含まれていない場合は false が return される
+         *
+         *  existsClubInRow
+         *    [false, 4, false, false, ... ,false] のようなデータになる
+         *
+         */
+        const existsClubInRow = data?.map((rowData, rowIndex) => {
+          return rowData.includes(clubId) && rowIndex;
+        });
+        /**
+         * @description
+         *  existsClubInRow の各要素 exists が false 以外(clubId と id が一致するクラブが存在する)の場合
+         *  sheet?.getRange(exists + 1, this.constants.SPREAD_SHEET.APPROVED_COLUMN_NUMBER).setValue(isApproved)
+         *  が実行される
+         *
+         *  result
+         *    [false, Range, false, false, ..., false] のようなデータになる
+         *
+         */
+        const results = existsClubInRow?.map((exists) => {
           return (
-            value[0] === clubId &&
-            sheet?.getRange(index, this.constants.SPREAD_SHEET.APPROVED_COLUMN_NUMBER).setValue(isApproved)
+            exists &&
+            sheet?.getRange(exists + 1, this.constants.SPREAD_SHEET.APPROVED_COLUMN_NUMBER).setValue(isApproved)
           );
         });
-        return this.view.provide(this.res.created);
-      }
-      return this.view.provide(this.res.internalServer);
-    } catch (error) {
-      console.error({ error });
-      return this.view.provide(this.res.internalServer);
-    }
-  }
-
-  private deleteRejectedClub(clubId: ApproveInterface["clubId"]) {
-    try {
-      const sheetTabName = PropertiesService.getScriptProperties().getProperty("SHEET_TAB_NAME");
-      if (sheetTabName) {
-        const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetTabName);
-        const data = sheet?.getDataRange().getValues();
-        data?.map((value, index) => {
-          /**
-           * value[0] : id
-           */
-          return value[0] === clubId && sheet?.deleteRow(index);
-        });
+        /**
+         * @description
+         *  Array.prototype.every
+         *    @see https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/every
+         *
+         *  results に false が含まれない(シートの更新が正常に終了した)場合
+         *  201 created を返す
+         *
+         */
+        if (results?.every((result) => typeof result === "boolean")) return this.view.provide(this.res.notFound);
         return this.view.provide(this.res.created);
       }
       return this.view.provide(this.res.internalServer);
