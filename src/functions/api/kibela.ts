16ce657732fecb175969b591da4af65a39607879
tk-ozawa <ozawa.shotaro0114@gmail.com>
2021-02-19 16:09:03 +0900

    update: KibelaAPIをexport

--- src/functions/api/kibela.ts
+++ src/functions/api/kibela.ts
@@ -4,11 +4,11 @@ import { ASTNode } from "graphql/language/ast";
 import gql from "graphql-tag";
 import type kibela from "../../../@types/kibela.d";
 
-const KibelaAPI = async (query: ASTNode): Promise<any> => {
-  const response = await nodeFetch(`https://${process.env.KIBELA_TEAM_NAME}.kibe.la/api/v1`, {
+export const KibelaAPI = async (query: ASTNode): Promise<any> => {
+  const response = await nodeFetch(`https://${process.env.KIBELA_TEAM}.kibe.la/api/v1`, {
     method: "POST",
     headers: {
-      Authorization: `Bearer ${process.env.KIBELA_ACCESS_TOKEN}`,
+      Authorization: `Bearer ${process.env.KIBELA_TOKEN}`,
       "Content-Type": "application/json",
       Accept: "application/json",
       "User-Agent": "KibelaAPITestFromCurl",
@@ -18,11 +18,11 @@ const KibelaAPI = async (query: ASTNode): Promise<any> => {
       variables: {},
     }),
   });
-
   if (!response.ok) {
     const body = await response.text();
     throw new Error(`not ok: ${response.statusText}\n${body}`);
   }
+
   const result = await response.json();
   return result.data;
 };
@@ -41,6 +41,5 @@ export const getGroup = async (): Promise<kibela.Group> => {
       }
     }
   `;
-
   return KibelaAPI(query);
 };
