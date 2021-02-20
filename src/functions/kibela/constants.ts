fcda56e614fe39cdc05eff70153058ddcf1ce765
tk-ozawa <ozawa.shotaro0114@gmail.com>
2021-02-20 20:07:03 +0900

    chore: 不要なファイル削除

--- src/functions/kibela/constants.ts
+++
@@ -1,9 +0,0 @@
-import * as dotenv from "dotenv";
-
-dotenv.config();
-
-export namespace Kibela {
-  export const TEAM_NAME = process.env.KIBELA_TEAM as string;
-  export const TOKEN = process.env.KIBELA_TOKEN as string;
-  export const END_POINT = `https://${TEAM_NAME}.kibe.la/api/v1`;
-}
