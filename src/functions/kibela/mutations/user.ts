5c365c9553d258ee007effcfeb5233b01e51cbc3
tk-ozawa <ozawa.shotaro0114@gmail.com>
2021-02-22 15:36:26 +0900

    add: kibelaユーザーを部活グループに入れるjoinGroup

--- src/functions/kibela/mutations/user.ts
+++ src/functions/kibela/mutations/user.ts
@@ -1,1 +1,14 @@
-export const addMemberToGroup = () => {};
+import gql from "graphql-tag";
+import { callAPI } from "../../api/kibela";
+
+export const joinGroup = async (userId: string, groupId: string) => {
+  const mutation = gql`
+    mutation {
+      joinGroup(input: {userId: "${userId}", groupId: "${groupId}"}) {
+        clientMutationId
+      }
+    }
+  `;
+
+  await callAPI(mutation);
+};
