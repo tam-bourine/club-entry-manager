efb507a9d07a8e7e16b2b0c34a59cda3e816509d
shuta13 <hollystarsun@gmail.com>
2021-02-24 12:50:18 +0900

    fix: 関数の引数で no-unused-var 出たので ignore

--- src/gas/shared/types/UtilityTypes.ts
+++ src/gas/shared/types/UtilityTypes.ts
@@ -3,6 +3,7 @@
  *  @see https://qiita.com/uhyo/items/80ce7c00f413c1d1be56
  *  AtLeast<3, number> -> [0,0,0] のように第一引数で指定した数値以上の長さに配列を固定する型
  */
+// eslint-disable-next-line no-unused-vars
 type Append<Elm, T extends unknown[]> = ((arg: Elm, ...rest: T) => void) extends (...args: infer T2) => void
   ? T2
   : never;
