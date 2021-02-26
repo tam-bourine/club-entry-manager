af79fb889e47f360c6f32f71ebf7d675dafada43
tk-ozawa <ozawa.shotaro0114@gmail.com>
2021-02-26 18:08:15 +0900

    update: ResponseInterfaceをgas.api.callNewClubに定義

--- src/api/gas/api.ts
+++ src/api/gas/api.ts
@@ -2,8 +2,9 @@ import fetch from "node-fetch";
 import { URL } from "url";
 import { CallNewClubArg } from "../../types/Messages";
 import { Config } from "../../constant";
+import ResponseInterface from "../../gas/shared/types/ResponseInterface";
 
-export const callNewClub = async (params: CallNewClubArg): Promise<any> => {
+export const callNewClub = async (params: CallNewClubArg): Promise<ResponseInterface> => {
   const fullUrl = new URL(`${Config.Gas.END_POINT}?action=regist`);
 
   const response = await fetch(fullUrl, {
