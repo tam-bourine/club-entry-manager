a87f4a3886064c97fd63970112188a6adc00e2e1
tk-ozawa <ozawa.shotaro0114@gmail.com>
2021-03-01 23:49:41 +0900

    change: 入部申請時に使う部活一覧データのidをchannelIdにした

--- src/gas/models/Clubs/GetModel.ts
+++ src/gas/models/Clubs/GetModel.ts
@@ -13,16 +13,13 @@ export default class GetModel {
       if (sheetTabName) {
         const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetTabName);
         const data = sheet?.getDataRange().getValues();
-        const clubs: ResponseInterface["clubs"] = [];
-        data?.map((value) => {
-          return clubs.push({
-            id: value[0],
-            name: value[1],
-          });
-        });
+        const clubs: ResponseInterface["clubs"] = data?.map((values: Array<string>) => ({
+          id: values[5],
+          name: values[1],
+        }));
         return this.view.provide({ ...this.res.ok, clubs });
       }
-      return this.view.provide(this.res.internalServer);
+      throw new Error("SHEET_TAB_NAMEが設定されていません");
     } catch (error) {
       console.error({ error });
       return this.view.provide(this.res.internalServer);
