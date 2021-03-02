36229275c0277f45fb52ea8a0ab14840138ba3f3
tk-ozawa <ozawa.shotaro0114@gmail.com>
2021-03-02 12:18:52 +0900

    update: 早期return

--- src/gas/models/Clubs/GetModel.ts
+++ src/gas/models/Clubs/GetModel.ts
@@ -10,18 +10,19 @@ export default class GetModel {
   fetchClubs() {
     try {
       const sheetTabName = PropertiesService.getScriptProperties().getProperty("SHEET_TAB_NAME");
-      if (sheetTabName) {
-        const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetTabName);
-        const data = sheet?.getDataRange().getValues();
-        const clubs: ResponseInterface["clubs"] = data
-          ?.filter((_, index) => index !== 0)
-          .map((values: Array<string>) => ({
-            id: values[5],
-            name: values[1],
-          }));
-        return this.view.provide({ ...this.res.ok, clubs });
+      if (!sheetTabName) {
+        throw new Error("SHEET_TAB_NAMEが設定されていません");
       }
-      throw new Error("SHEET_TAB_NAMEが設定されていません");
+
+      const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetTabName);
+      const data = sheet?.getDataRange().getValues();
+      const clubs: ResponseInterface["clubs"] = data
+        ?.filter((_, index) => index !== 0)
+        .map((values: Array<string>) => ({
+          id: values[5],
+          name: values[1],
+        }));
+      return this.view.provide({ ...this.res.ok, clubs });
     } catch (error) {
       console.error({ error });
       return this.view.provide(this.res.internalServer);
