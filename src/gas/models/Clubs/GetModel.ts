8920ca42d2848d69b60117e19f9845204aede918
kenta chihara <kenta.chihara@tam-bourine.co.jp>
2021-03-03 14:12:06 +0900

    refactor: use constants

--- src/gas/models/Clubs/GetModel.ts
+++ src/gas/models/Clubs/GetModel.ts
@@ -1,3 +1,4 @@
+import Constants from "../../shared/Constants";
 import Response from "../../shared/Response";
 import ResponseInterface from "../../shared/types/ResponseInterface";
 import GetView from "../../views/Clubs/GetView";
@@ -7,6 +8,8 @@ export default class GetModel {
 
   private view = new GetView();
 
+  private constants = new Constants();
+
   fetchClubs() {
     try {
       const sheetTabName = PropertiesService.getScriptProperties().getProperty("SHEET_TAB_NAME");
@@ -14,13 +17,16 @@ export default class GetModel {
         throw new Error("SHEET_TAB_NAMEが設定されていません");
       }
 
+      const slackChannelIdArrayNumber = this.constants.SPREAD_SHEET.CLUBS.SLACK_CHANNEL_ID_COLUMN_NUMBER - 1;
+      const clubNameArrayNumber = this.constants.SPREAD_SHEET.CLUBS.CLUB_NAME_COLUMN_NUMBER - 1;
+
       const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetTabName);
       const data = sheet?.getDataRange().getValues();
       const clubs: ResponseInterface["clubs"] = data
         ?.filter((_, index) => index !== 0)
         .map((values: Array<string>) => ({
-          id: values[5],
-          name: values[1],
+          id: values[slackChannelIdArrayNumber],
+          name: values[clubNameArrayNumber],
         }));
       return this.view.provide({ ...this.res.ok, clubs });
     } catch (error) {
