110b2269d5141deccd628634fcea56b7f6e884de
shuta13 <hollystarsun@gmail.com>
2021-03-18 17:33:24 +0900

    update: approve controller types

--- src/__tests__/gas/controllers/ApproveController.test.ts
+++ src/__tests__/gas/controllers/ApproveController.test.ts
@@ -1,10 +1,11 @@
 import ApproveController from "../../../gas/controllers/Club/ApproveController";
 import ApproveModel from "../../../gas/models/Club/ApproveModel";
+import ApproveInterface from "../../../gas/shared/types/ApproveInterface";
 
 describe("ApproveController", () => {
   const controller = new ApproveController();
   const model = new ApproveModel();
-  const params = {
+  const params: ApproveInterface = {
     slackChannelId: "SLACK_CHANNEL_ID",
     authorizer: {
       slackId: "SLACK_ID",
