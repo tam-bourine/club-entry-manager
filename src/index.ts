5595a549e95b397477a60de4b2664a900f8740c4
kenta chihara <kenta.chihara@tam-bourine.co.jp>
2021-03-31 19:41:57 +0900

    refactor: revival conflict files

---
+++ src/index.ts
@@ -0,0 +1,59 @@
+import { App, ExpressReceiver } from "@slack/bolt";
+import { ChatPostMessageArguments } from "@slack/web-api";
+import { Config } from "./constant";
+import * as slackAPI from "./api/slack";
+import { enableNewClubCommand } from "./commands/newClub";
+import { enableNewClubShortcut } from "./shortcuts/newClub";
+import { enableJoinClubShortcut } from "./shortcuts/joinClub";
+
+const expressReceiver = new ExpressReceiver({
+  signingSecret: Config.Slack.SIGNING_SECRET,
+  endpoints: "/slack/events",
+  processBeforeResponse: true,
+});
+
+export const app = new App({
+  socketMode: Config.General.APP_ENV === Config.General.APP_ENV_TYPE.LOCAL,
+  appToken: Config.General.APP_ENV === Config.General.APP_ENV_TYPE.LOCAL ? Config.Slack.APP_TOKEN : undefined,
+  token: Config.Slack.BOT_TOKEN,
+  receiver:
+    Config.General.APP_ENV === Config.General.APP_ENV_TYPE.DEV ||
+    Config.General.APP_ENV === Config.General.APP_ENV_TYPE.PRD
+      ? expressReceiver
+      : undefined,
+  processBeforeResponse:
+    Config.General.APP_ENV === Config.General.APP_ENV_TYPE.DEV ||
+    Config.General.APP_ENV === Config.General.APP_ENV_TYPE.PRD,
+});
+
+app.error((err) => {
+  return new Promise(() => {
+    console.error({ err });
+  });
+});
+
+enableNewClubShortcut(app);
+enableJoinClubShortcut(app, Config.Slack.APPROVAL_CHANNEL_ID);
+enableNewClubCommand(app, Config.Slack.APPROVAL_CHANNEL_ID);
+
+(async () => {
+  await app.start(Config.Slack.Bolt.SERVE_PORT);
+  console.log(`⚡️ Bolt app is running! on PORT: ${Config.Slack.Bolt.SERVE_PORT}!`);
+
+  if (Config.General.APP_ENV === Config.General.APP_ENV_TYPE.LOCAL) {
+    const userResult = await slackAPI.user.getById(Config.Slack.Bolt.DEBUG_USER);
+    const name = userResult.profile?.display_name || userResult.real_name;
+    const icon = userResult.profile?.image_original;
+
+    const msgOption: ChatPostMessageArguments = {
+      token: Config.Slack.BOT_TOKEN,
+      text: "アプリ起動なう :zap:",
+      channel: Config.Slack.DEBUG_CHANNEL_ID,
+      icon_url: icon,
+      username: name,
+    };
+
+    console.info(JSON.stringify(msgOption, null, 2));
+    await app.client.chat.postMessage(msgOption);
+  }
+})();
---
+++ src/shortcuts/joinClub.ts
@@ -0,0 +1,119 @@
+import { AllMiddlewareArgs, App, SlackShortcutMiddlewareArgs } from "@slack/bolt";
+import { Modal } from "../config/modalConfig";
+import { openModal, openAlertModal } from "../modal/modalTemplate";
+import { ErrorAlert } from "../config/errorConfig";
+import { Club } from "../config/clubConfig";
+import { sectionPlainText } from "../blocks/generalComponents";
+import { getJoinClubBlocks } from "../blocks/joinClub";
+import * as gas from "../api/gas";
+// import * as kibela from "../api/kibela";
+import * as slack from "../api/slack";
+
+export const enableJoinClubShortcut = (app: App, approvalChannelId: string) => {
+  app.shortcut(
+    "open_join_club_modal",
+    async ({ ack, body, client, context: { botToken } }: SlackShortcutMiddlewareArgs & AllMiddlewareArgs) => {
+      await ack();
+
+      const response = await gas.api.callNewJoinClub();
+      if (!response.success) {
+        await client.chat
+          .postMessage({
+            channel: approvalChannelId,
+            text: ErrorAlert.text.NOTIFICATION,
+            blocks: [sectionPlainText({ title: Club.label.ERROR, text: ErrorAlert.text.CONTACT_DEVELOPER })],
+          })
+          .catch((error) => {
+            throw new Error(error);
+          });
+        return;
+      }
+
+      const { clubs } = response;
+      if (!clubs) {
+        await openAlertModal({
+          client,
+          botToken,
+          triggerId: body.trigger_id,
+          title: Modal.title.NO_CLUB,
+          text: ErrorAlert.text.NO_EXIST_CLUB,
+          imageUrl: ErrorAlert.image.SORRY,
+        });
+        return;
+      }
+
+      const injectClubs = clubs.map(({ id, name }) => ({
+        text: name,
+        value: id,
+      }));
+      openModal({
+        client,
+        botToken,
+        triggerId: body.trigger_id,
+        callbackId: Modal.id.JOIN_CLUB_VIEWS_ID,
+        title: Modal.title.JOIN,
+        blocks: getJoinClubBlocks(injectClubs),
+        submit: Modal.button.REQUEST,
+      });
+    }
+  );
+
+  app.view(
+    Modal.id.JOIN_CLUB_VIEWS_ID,
+    async ({
+      ack,
+      view: {
+        state: { values },
+      },
+      client,
+      body: {
+        user: { id: slackUserId },
+      },
+    }) => {
+      await ack();
+
+      const { value: clubRecordId }: { value: string } = values.join_input.join.selected_option;
+
+      const member = await slack.user.getById(slackUserId);
+      // const kibelaUsers = await kibela.query.user.getAll();
+      // const userKibelaInfo = await kibela.query.user.findByEmail(member.profile.email, kibelaUsers);
+
+      const response = await gas.api.callJoinClub({
+        club: {
+          id: clubRecordId,
+        },
+        member: {
+          slackId: member.id,
+          name: member.real_name,
+        },
+      });
+
+      const { success, club } = response;
+      if (!success || !club || !club.kibelaUrl || !club.channelId) {
+        await client.chat
+          .postMessage({
+            channel: approvalChannelId,
+            text: ErrorAlert.text.NOTIFICATION,
+            blocks: [sectionPlainText({ title: Club.label.ERROR, text: ErrorAlert.text.CONTACT_DEVELOPER })],
+          })
+          .catch((error) => {
+            console.error({ error });
+          });
+        return;
+      }
+
+      const { kibelaUrl, channelId } = club;
+
+      await Promise.all([
+        client.conversations.invite({
+          channel: channelId,
+          users: slackUserId,
+        }),
+        // (async () => {
+        //   const group = await kibela.query.group.getByNoteUrl(kibelaUrl);
+        //   kibela.mutation.user.joinGroup(userKibelaInfo.id, group.id);
+        // })(),
+      ]);
+    }
+  );
+};
